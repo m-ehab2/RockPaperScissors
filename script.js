1c22435763c89d1251c1153f600503b3a570a9e8
m.ehab2 <muhamedehab1@gmail.com>
2023-03-21 00:20:05 +0200

    rps-ui

--- script.js
+++ script.js
@@ -1,74 +1,77 @@
 function getComputerChoice(){
-    let x=Math.floor(Math.random() * 3);
-    if(x===0){
-        return x;
+    let x=Math.floor(Math.random() * 100);
+    if(x<33){
+        return 0;
     }
-    else if(x===1){
-        return x;
+    else if(x<66){
+        return 1;
     }
     else {
         return 2;
     }
 }
 
-function playRound(){
-    let p=prompt("So, Rock, Paper or Scissors?")
-    p=p.toLowerCase();
+let x=0;
+let y=0
+function pick(color){
+    console.log(color);
     c=getComputerChoice();
+    console.log(c);
+    p=color;
+    let cs=document.querySelector(".cscore").querySelector(".score");
+    let ps=document.querySelector(".pscore").querySelector(".score");
+    let cp=document.querySelector(".cpick").querySelector(".pick");
+    let pp=document.querySelector(".ppick").querySelector(".pick");
+    cp.innerHTML=c;
+    pp.innerHTML=p;
     if(p==="rock"){
         if(c===0){
-            window.alert("It's A tie");
-            return 0;
+            cp.innerHTML="rock";
         }
         else if(c===1){
-            window.alert("You Lost, Unlucky!");
-            return -1;
+            cp.innerHTML="paper";
+            x++;
+            cs.innerHTML=x;
         }
         else{
-            window.alert("Congrats You won This Round");
-            return 1;
+            cp.innerHTML="scissors";
+            y++;
+            ps.innerHTML=y;
         }
     }
     else if(p==="paper"){
         if(c===0){
-            window.alert("Congrats You won This Round");
-            return 1;
+            cp.innerHTML="rock";
+            y++;
+            ps.innerHTML=y;
         }
         else if(c===1){
-            window.alert("It's A tie");
-            return 0;
+            cp.innerHTML="paper";
         }
         else{
-            window.alert("You Lost, Unlucky!");
-            return -1;
+            cp.innerHTML="scissors";
+            x++;
+            cs.innerHTML=x;
         }
     }
     else if(p==="scissors"){
+        console.log(`picked scissors`);
         if(c===0){
-            window.alert("You Lost, Unlucky!");
-            return -1;
+            cp.innerHTML="rock";
+            x++;
+            cs.innerHTML=x;
         }
         else if(c===1){
-            window.alert("Congrats You won This Round");
-            return 1;
+            cp.innerHTML="paper";
+            y++;
+            ps.innerHTML=y;
         }
         else{
-            window.alert("It's A tie");
-            return 0;
+            cp.innerHTML="scissors";
         }
     }
     else{
-        window.alert("Excuse me? you get a -1 for the typo")
         return -1;
     }
 
 }
-function game(){
-    let m=prompt("How many round would you like to play?");
-    let score=0;
-    for (let i=0;i<m;i++){
-        score=score+playRound();
-        window.alert(`Score is ${score}`);
-    }
-}
-game();
